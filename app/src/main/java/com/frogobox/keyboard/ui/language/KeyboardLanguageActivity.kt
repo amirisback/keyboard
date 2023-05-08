c9960fdcf2baaf211a054ce63af796d879ebccf8
Muhammad Faisal Amir <faisalamircs@gmail.com>
2023-05-08 16:16:01 +0700

    update: setup keyboard language change
    Co-authored-by: frogoboxbot <frogoboxmedia@gmail.com>

--- app/src/main/java/com/frogobox/keyboard/ui/language/KeyboardLanguageActivity.kt
+++ app/src/main/java/com/frogobox/keyboard/ui/language/KeyboardLanguageActivity.kt
@@ -1,9 +1,16 @@
 package com.frogobox.keyboard.ui.language
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.viewModels
 import com.frogobox.keyboard.common.base.BaseActivity
 import com.frogobox.keyboard.databinding.ActivityKeyboardLanguageBinding
+import com.frogobox.keyboard.databinding.ItemLanguageKeyboardBinding
+import com.frogobox.recycler.core.FrogoRecyclerNotifyListener
+import com.frogobox.recycler.core.IFrogoBindingAdapter
+import com.frogobox.recycler.ext.injectorBinding
+import com.frogobox.sdk.ext.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -17,7 +24,8 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 
 @AndroidEntryPoint
-class KeyboardLanguageActivity : BaseActivity<ActivityKeyboardLanguageBinding>() {
+class KeyboardLanguageActivity : BaseActivity<ActivityKeyboardLanguageBinding>(),
+    IFrogoBindingAdapter<KeyboardLanguage, ItemLanguageKeyboardBinding> {
 
     private val viewModel: KeyboardLanguageViewModel by viewModels()
 
@@ -28,19 +36,67 @@ class KeyboardLanguageActivity : BaseActivity<ActivityKeyboardLanguageBinding>()
     override fun setupViewModel() {
         super.setupViewModel()
         viewModel.apply {
-
+            keyboardLanguage.observe(this@KeyboardLanguageActivity) {
+                setupRV(it)
+            }
         }
     }
 
     override fun onCreateExt(savedInstanceState: Bundle?) {
         super.onCreateExt(savedInstanceState)
-        setupUI()
+        if (savedInstanceState == null) {
+            viewModel.getKeyboardLanguage(this)
+        }
     }
 
-    private fun setupUI() {
+    override fun initView() {
+        super.initView()
+        setupDetailActivity("Keyboard Language")
         binding.apply {
 
         }
+    }
+
+    override fun setViewBinding(parent: ViewGroup): ItemLanguageKeyboardBinding {
+        return ItemLanguageKeyboardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    }
+
+    override fun onItemClicked(
+        binding: ItemLanguageKeyboardBinding,
+        data: KeyboardLanguage,
+        position: Int,
+        notifyListener: FrogoRecyclerNotifyListener<KeyboardLanguage>
+    ) {
+        viewModel.setKeyboard(data.xml) {
+            showToast("${data.name} Selected")
+        }
+    }
+
+    override fun onItemLongClicked(
+        binding: ItemLanguageKeyboardBinding,
+        data: KeyboardLanguage,
+        position: Int,
+        notifyListener: FrogoRecyclerNotifyListener<KeyboardLanguage>
+    ) {}
+
+    override fun setupInitComponent(
+        binding: ItemLanguageKeyboardBinding,
+        data: KeyboardLanguage,
+        position: Int,
+        notifyListener: FrogoRecyclerNotifyListener<KeyboardLanguage>
+    ) {
+        binding.apply {
+            tvItemKeyboardMain.text = data.name
+        }
+    }
+
+    private fun setupRV(data: List<KeyboardLanguage>) {
+        binding.rvKeyboardLanguage
+            .injectorBinding<KeyboardLanguage, ItemLanguageKeyboardBinding>()
+            .addData(data)
+            .addCallback(this)
+            .createLayoutLinearVertical(false)
+            .build()
     }
 
 }
--- app/src/main/java/com/frogobox/keyboard/ui/language/KeyboardLanguageViewModel.kt
+++ app/src/main/java/com/frogobox/keyboard/ui/language/KeyboardLanguageViewModel.kt
@@ -1,6 +1,13 @@
 package com.frogobox.keyboard.ui.language
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.frogobox.coresdk.response.FrogoStateResponse
+import com.frogobox.keyboard.R
 import com.frogobox.keyboard.common.base.BaseViewModel
+import com.frogobox.keyboard.util.Constant
+import com.frogobox.sdk.delegate.preference.PreferenceDelegates
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -17,8 +24,49 @@ import javax.inject.Inject
 
 @HiltViewModel
 class KeyboardLanguageViewModel @Inject constructor(
+    private val pref : PreferenceDelegates
+) : BaseViewModel() {
 
-): BaseViewModel() {
+    private var _keyboardLanguage = MutableLiveData<List<KeyboardLanguage>>()
+    var keyboardLanguage: LiveData<List<KeyboardLanguage>> = _keyboardLanguage
 
+    private var _showloading = MutableLiveData<Boolean>()
+    var showloading: LiveData<Boolean> = _showloading
+
+    fun getKeyboardLanguage(context: Context) {
+        val list = mutableListOf<KeyboardLanguage>()
+        list.add(KeyboardLanguage("${context.getString(R.string.translation_english)} (QWERTY)", R.xml.keys_letters_qwerty))
+        list.add(KeyboardLanguage("${context.getString(R.string.translation_english)} (QWERTZ)", R.xml.keys_letters_english_qwertz))
+        list.add(KeyboardLanguage("${context.getString(R.string.translation_english)} (DVORAK)", R.xml.keys_letters_english_dvorak))
+
+        list.add(KeyboardLanguage(context.getString(R.string.translation_bengali), R.xml.keys_letters_bengali))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_bulgarian), R.xml.keys_letters_bulgarian))
+
+        list.add(KeyboardLanguage(context.getString(R.string.translation_french), R.xml.keys_letters_french))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_german), R.xml.keys_letters_german))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_greek), R.xml.keys_letters_greek))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_lithuanian), R.xml.keys_letters_lithuanian))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_romanian), R.xml.keys_letters_romanian))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_slovenian), R.xml.keys_letters_slovenian))
+        list.add(KeyboardLanguage(context.getString(R.string.translation_spanish), R.xml.keys_letters_spanish_qwerty))
+        list.add(KeyboardLanguage("${context.getString(R.string.translation_turkish)} (Q)", R.xml.keys_letters_turkish_q))
+        _keyboardLanguage.postValue(list)
+    }
+
+    fun setKeyboard(xml: Int, onSuccess: () -> Unit) {
+        pref.savePrefInt(Constant.PREF_KEYBOARD_TYPE, xml, object : FrogoStateResponse {
+            override fun onFailed(statusCode: Int, errorMessage: String) {}
+            override fun onFinish() {}
+            override fun onHideProgress() {
+                _showloading.postValue(false)
+            }
+            override fun onShowProgress() {
+                _showloading.postValue(true)
+            }
+            override fun onSuccess() {
+                onSuccess()
+            }
+        })
+    }
 
 }

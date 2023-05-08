66947047377fdf88df316b8f399ea6a9e1b9b52b
Faisal Amir <faisalamircs@gmail.com>
2023-05-08 17:06:54 +0700

    Update ProjectSetting.kt

--- buildSrc/src/main/kotlin/ProjectSetting.kt
+++ buildSrc/src/main/kotlin/ProjectSetting.kt
@@ -24,7 +24,7 @@ object ProjectSetting {
 
     const val VERSION_MAJOR = 1
     const val VERSION_MINOR = 0
-    const val VERSION_PATCH = 8
+    const val VERSION_PATCH = 9
 
     // ---------------------------------------------------------------------------------------------
 
